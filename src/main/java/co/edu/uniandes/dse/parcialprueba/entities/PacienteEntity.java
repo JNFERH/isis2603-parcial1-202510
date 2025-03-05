47754105cf9c23dc81a2366528454b4eb56ca46b
Felipe Rosero <jf.rosero@uniandes.edu.co>
2025-03-05 15:47:28 -0500

    Entidades

---
+++ src/main/java/co/edu/uniandes/dse/parcialprueba/entities/PacienteEntity.java
@@ -0,0 +1,31 @@
+package co.edu.uniandes.dse.parcialprueba.entities;
+
+import java.util.ArrayList;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
+
+import java.util.List;
+
+@Data
+@Entity
+public class PacienteEntity extends BaseEntity {
+
+    private String nombre;
+    private String correo;
+    private Integer telefono;
+
+    @PodamExclude
+    @OneToOne
+    private PacienteEntity acudiente;
+
+    @PodamExclude
+    @OneToMany(mappedBy= "paciente", cascade= CascadeType.PERSIST, orphanRemoval = true)
+    private List<HistoriaClinicaEntity> historiasClinicas= new ArrayList<>();
+
+
+}
